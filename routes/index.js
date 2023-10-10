95554dfc00be43097b384dd060a89fdce04c4bf4
Thang-Truong <thangtruong1808@gmail.com>
2023-10-10 22:17:10 +1100

    applied express-ejs-layouts

--- routes/index.js
+++ routes/index.js
@@ -4,13 +4,13 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function (req, res, next) {
   // console.log("Using nodemon auto restart server");
-  res.render('index', { title: 'HomePage' });
+  res.render('index', { pageTitle: 'Welcome to HomePage', title:"HomePage" });
 });
 
 /* GET dashboard page. */
 router.get('/dashboard', function (req, res, next) {
   // console.log("Using nodemon auto restart server");
-  res.render('pages/dashboard/index', { title: 'Dashboard Page' });
+  res.render('pages/dashboard/index', { pageTitle: 'Welcome to Dashboard ', title:"Dashboard" });
 });
 
 module.exports = router;
--- routes/items.js
+++ routes/items.js
@@ -3,12 +3,12 @@ var router = express.Router();
 
 /* GET item-list. */
 router.get('/list', function(req, res, next) {
-    res.render('pages/items/list.ejs', { title: 'Welcome to Item List Page' });
+    res.render('pages/items/list.ejs', { pageTitle: 'Welcome to List Page', title:"List Page"});
 });
 
 /* GET item-list. */
 router.get('/add', function(req, res, next) {
-    res.render('pages/items/add.ejs', { title: 'Welcome to Item Add Page' });
+    res.render('pages/items/add.ejs', { pageTitle: 'Welcome to Add Item Page', title:"Add Item Page" });
 });
 
 module.exports = router;
