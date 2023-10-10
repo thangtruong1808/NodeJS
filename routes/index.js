c25c18852d1e8bcee1d43443e3e416f0a2e50948
Thang-Truong <thangtruong1808@gmail.com>
2023-10-10 14:31:16 +1100

    updated nodemon auto restart server

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
+  // console.log("Using nodemon auto restart server");
   res.render('index', { title: 'Express' });
 });
 
