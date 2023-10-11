bd6db6618f2253a0eab69b46e9575a5dc4b95dcb
Thang-Truong <thangtruong1808@gmail.com>
2023-10-11 16:52:18 +1100

    updated Error Page

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render('pages/error', {pageTitle: 'Page Not Found !',  title: "Error Page"});
 });
 
 module.exports = app;
