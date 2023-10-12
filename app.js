ea27f1d91c05281df0536e1866eedacfe161d469
Thang-Truong <thangtruong1808@gmail.com>
2023-10-13 02:59:12 +1100

    Inserted records into MongoDB Successfully

--- app.js
+++ app.js
@@ -10,17 +10,80 @@ const mongoose = require('mongoose');
 
 const systemConfig = require('./configs/system')
 
-mongoose.connect('mongodb+srv://oliverthangtruong:cvrnjjPt96sKnj16@nodejs.9fbzhbv.mongodb.net/?retryWrites=true&w=majority');
 
-var db = mongoose.connection;
 
-db.on('error', () => {
-  console.log("Connection Error by THANG-TRUONG");
-});
 
-db.once('open', () => {
-  console.log("Connection Connected Successfully !");
-})
+
+main().catch(err => console.log("Connection to Database Error ! ! ! "));
+
+async function main() {
+  await mongoose.connect('mongodb+srv://oliverthangtruong:cvrnjjPt96sKnj16@nodejs.9fbzhbv.mongodb.net/NodeJSDB');
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+
+  //  const itemSchema = new mongoose.Schema({
+  //   name: String
+  //  });
+  
+  const kittySchema = new mongoose.Schema({
+    name: String
+  });
+
+    // NOTE: methods must be added to the schema before compiling it with mongoose.model()
+  kittySchema.methods.speak = function speak() {
+    const greeting = this.name
+      ? 'Meow name is ' + this.name
+      : 'I don\'t have a name';
+    console.log(greeting);
+  };
+  // const myItem = mongoose.model('Item', itemSchema);
+  const Kitten = mongoose.model('Kitten', kittySchema);
+
+  // const thangtruong = new myItem({ name: 'THANG-TRUONG' });
+  const fluffy = new Kitten({ name: 'fluffy' });
+  fluffy.speak();
+
+  // await thangtruong.save();
+  await fluffy.save();  
+   fluffy.speak();
+  // const myResult = await myItem.find();
+  // const myResult2 = await Kitten.find();
+  // // console.log(myResult);
+  // console.log(myResult2);
+}
+
+
+// mongoose.connect('mongodb+srv://oliverthangtruong:cvrnjjPt96sKnj16@nodejs.9fbzhbv.mongodb.net/?retryWrites=true&w=majority');
+
+// var db = mongoose.connection;
+
+// db.on('error', () => {
+//   console.log("Connection Error by THANG-TRUONG");
+// });
+
+// db.once('open', () => {
+//   console.log("Connection Connected Successfully !");
+// })
+
+// const kittySchema = new mongoose.Schema({
+//   name: String
+// });
+
+// kittySchema.methods.speak = function speak() {
+//   const greeting = this.name
+//     ? 'Meow name is ' + this.name
+//     : 'I don\'t have a name';
+//   console.log(greeting);
+// };
+
+// const Kitten = mongoose.model('Kitten', kittySchema);
+
+// const silence = new Kitten({ name: 'Silence' });
+// // console.log(silence.name); // 'Silence
+// // silence.save(function (err, silence) {
+// //   if (err) return console.log("Error 2");
+
+
+
 
 // var indexRouter = require('./routes/index');
 // var ItemsRouter = require('./routes/items');
