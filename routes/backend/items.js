72b7949d38f84288e8d33e10f5acda50cab7b956
Thang-Truong <thangtruong1808@gmail.com>
2023-10-23 02:41:47 +1100

    Completed Pagination

--- routes/backend/items.js
+++ routes/backend/items.js
@@ -12,13 +12,13 @@ router.get("(/:status)?", (req, res, next) => {
   let keyword = ParamHelpers.getParam(req.query, "keyword", "");
   let currentStatus = ParamHelpers.getParam(req.params, "status", "all");
   let statusFilter = UtilsHelpers.createFilterStatus(currentStatus);
-  let pagination = {
+  let paginationObj = {
     totalItems: 1,
-    totalItemsPerPage: 2,
+    totalItemsPerPage: 8,
     currentPage: 1,
     pageRanges: 5,
   };
-  pagination.currentPage = parseInt(
+  paginationObj.currentPage = parseInt(
     ParamHelpers.getParam(req.query, "page", "1")
   );
 
@@ -29,18 +29,18 @@ router.get("(/:status)?", (req, res, next) => {
   }
 
   ItemsModel.countDocuments(objWhere).then((data) => {
-    pagination.totalItems = data;
+    paginationObj.totalItems = data;
     ItemsModel.find(objWhere)
       .sort({ name: "asc" })
-      .skip((pagination.currentPage - 1) * pagination.totalItemsPerPage)
-      .limit(pagination.totalItemsPerPage)
+      .skip((paginationObj.currentPage - 1) * paginationObj.totalItemsPerPage)
+      .limit(paginationObj.totalItemsPerPage)
       .then((items) => {
         res.render("pages/items/list", {
           pageTitle: "Welcome to List Page",
           title: "List Page",
           items: items,
           statusFilter: statusFilter,
-          pagination,
+          paginationObj,
           currentStatus,
           keyword,
         });
